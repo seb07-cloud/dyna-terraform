59d4465137405dbeefc3f5f1a5d7e1e6192fbccc
seb07 <wild.sebastian@outlook.de>
2022-06-15 14:18:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,25 +9,25 @@ terraform {
   }
 }
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.9.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.22.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.2.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.7.2"
-    }
+required_providers {
+  azurerm = {
+    source  = "hashicorp/azurerm"
+    version = "=3.9.0"
+  }
+  azuread = {
+    source  = "hashicorp/azuread"
+    version = "~> 2.22.0"
+  }
+  random = {
+    source  = "hashicorp/random"
+    version = "~> 3.2.0"
+  }
+  time = {
+    source  = "hashicorp/time"
+    version = "~> 0.7.2"
   }
 }
+
 
 provider "azurerm" {
   features {}
